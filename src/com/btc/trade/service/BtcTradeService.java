8cf48b2a06efa9cbd926bcb2a7c19f5926be4943
jidu <jdu@hp.com>
2013-03-30 13:52:40 +0800

    add some method

--- src/com/btc/trade/service/BtcTradeService.java
+++ src/com/btc/trade/service/BtcTradeService.java
@@ -2,4 +2,7 @@ package com.btc.trade.service;
 
 public interface BtcTradeService {
 
+	public String buy();
+	
+	public String sell();
 }
--- src/com/btc/trade/service/impl/BtcTradeServiceImpl.java
+++ src/com/btc/trade/service/impl/BtcTradeServiceImpl.java
@@ -4,4 +4,16 @@ import com.btc.trade.service.BtcTradeService;
 
 public class BtcTradeServiceImpl implements BtcTradeService{
 
+	@Override
+	public String buy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String sell() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
